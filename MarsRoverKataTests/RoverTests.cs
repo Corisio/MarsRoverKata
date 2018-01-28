d5ad2a24ffd5383d4599f1d0e831d13e2d048262
Rubén Aguilar Becerra <ruben.aguilar.becerra@outlook.es>
2018-01-28 21:46:22 +0100

    First implementation using switch

--- MarsRoverKataTests/RoverTests.cs
+++ MarsRoverKataTests/RoverTests.cs
@@ -4,11 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using MarsRoverkata;
 
 namespace MarsRoverKataTests
 {
     class RoverShould
     {
+        private const char INITIAL_DIRECTION = 'N';
 
+        private const int INITIAL_POSITION_X = 0;
+        private const int INITIAL_POSITION_Y = 0;
+
+        private const char FORWARD_COMMAND = 'f';
+        private const char BACKWARD_COMMAND = 'b';
+
+        private Rover rover;
+
+        [SetUp]
+        public void SetUp()
+        {
+            rover = new Rover();
+        }
+
+        [Test]
+        public void has_an_initial_position()
+        {
+            Assert.AreEqual(INITIAL_POSITION_X, rover.Position.X);
+            Assert.AreEqual(INITIAL_POSITION_Y, rover.Position.Y);
+        }
+
+        [Test]
+        public void has_an_initial_direction()
+        {
+            Assert.AreEqual(INITIAL_DIRECTION, rover.Direction);
+        }
+
+        [Test]
+        public void moves_forward_when_receives_forward_command()
+        {
+            rover.Execute(new List<char> { FORWARD_COMMAND });
+
+            Assert.AreEqual(1, rover.Position.Y);
+        }
+
+        [Test]
+        public void moves_backwards_when_receives_backward_command()
+        {
+            rover.Execute(new List<char> { BACKWARD_COMMAND });
+
+            Assert.AreEqual(-1, rover.Position.Y);
+        }
     }
 }
--- MarsRoverkata/Rover.cs
+++ MarsRoverkata/Rover.cs
@@ -8,6 +8,45 @@ namespace MarsRoverkata
 {
     public class Rover
     {
+        private const char INITIAL_DIRECTION = 'N';
 
+        private const char FORWARD_COMMAND = 'f';
+        private const char BACKWARD_COMMAND = 'b';
+
+        public Rover()
+        {
+            Position = new Position();
+            Direction = INITIAL_DIRECTION;
+        }
+
+        public Position Position { get; set; }
+
+        public char Direction { get; set; }
+
+        public void Execute(List<char> list)
+        {
+            foreach(var command in list)
+            {
+                switch(command)
+                {
+                    case FORWARD_COMMAND:
+                        MoveForward();
+                        break;
+                    case BACKWARD_COMMAND:
+                        MoveBackwards();
+                        break;
+                }
+            }
+        }
+
+        private void MoveBackwards()
+        {
+            Position.Y--;
+        }
+
+        private void MoveForward()
+        {
+            Position.Y++;
+        }
     }
 }
